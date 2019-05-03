3270d34f7dfff4e99c71fe5e16302ad5b1dce241
Arne Otten <arne.otten90@googlemail.com>
2019-05-03 21:58:53 +0200

    doorsign.php passt sich mit ID an

---
+++ app/doorsign_methods.php
@@ -0,0 +1,36 @@
+<?php
+const CONNECTION_FAILED = 100;
+const DB_DOES_NOT_EXISTS = 103;
+
+function connect(){
+    $configs = include ('config.php');
+    $path = $configs['PATH_TO_SQLITE_FILE'];
+
+    if (file_exists($path)) {
+        try {
+            $dbh = new PDO("sqlite:" . $path);
+            return $dbh;
+        } 
+        catch (PDOException $e) {
+            return CONNECTION_FAILED;
+        }
+    } else {
+        return DB_DOES_NOT_EXISTS;
+    }
+}
+
+function getRoom($id){
+    //Connect to DB
+    $pdo = connect();
+    if(!($pdo instanceof PDO)){
+        throw new Exception("Verbindung zur DB gescheitert!");
+    }
+
+    $sql = 'SELECT Roomname, Line1, Line2, Line3, Occupied FROM Room WHERE RId IS :id';
+    $statement = $pdo->prepare($sql);
+    
+    $statement->execute([':id' => $id]);
+    return $statement->fetch(PDO::FETCH_ASSOC);
+}
+
+?>
--- doorsign.php
+++ doorsign.php
@@ -1,3 +1,19 @@
+<?php 
+include "./app/doorsign_methods.php";
+
+if(isset($_GET["id"])){
+  $room = getRoom($_GET["id"]);
+  $roomname = $room["Roomname"];
+  $line1 = $room["Line1"];
+  $line2 = $room["Line2"];
+  $occupied = $room["Occupied"];
+} else {
+  $roomname = "Roomname - Call with proper Room ID";
+  $line1 = "";
+  $line2 = "";
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +23,7 @@
   <meta charset="utf-8">
   <title>Your page title here :)</title>
   <meta name="description" content="">
-  <meta name="author" content="">
+  <meta name="author" content="Arne Otten">
 
   <!-- Mobile Specific Metas
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
@@ -24,6 +40,10 @@
   <link rel="stylesheet" href="css/skeleton.css">
   <link rel="stylesheet" href="css/signstyle.css">
 
+  <!-- JS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <script type="text/javascript" src="js/jquery-3.3.1.js"></script>
+
   <!-- Favicon
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <link rel="icon" type="image/png" href="images/favicon.png">
@@ -33,16 +53,17 @@
 
   <!-- Primary Page Layout
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <div class="line">
-    <h1><b>Konferenzraum "Tullum" - Frei</b></h4>       
+  <div class="line" style="<?php if(isset($_GET["id"])) echo ($occupied) ? "background-color: darkred;" : "background-color: green;";?>">
+    <h1><b id="roomname" ><?php echo htmlspecialchars($roomname); if(isset($_GET["id"])) echo ($occupied) ? " - Belegt" : " - Frei"; ?></b></h1>       
+    <input id="constRoomname" type="hidden" value="<?php echo htmlspecialchars($roomname); ?>">
   </div>
 
   <div class="container">
     <div class="row">
       <div class="twelve columns">
-        <div class="status">
-          <h2>08:00 - 10:00 Frühstücksrunde</h2>
-          <h2>10:00 - 13:00 IHK Prüfung</h2>
+        <div class="status" >
+          <h2><?php echo htmlspecialchars($line1); ?></h2>
+          <h2><?php echo htmlspecialchars($line2); ?></h2>
         </div>
       </div>
     </div>
@@ -54,8 +75,36 @@
   </div>
 
   <div class="switch">
-    <button class="button-primary">Belegt</button>
+    <button onclick="change()" id="switch" class="button-primary" style="<?php if(isset($_GET["id"])) echo ($occupied) ? "background-color: darkred;border-color: darkred;" : "background-color: green;border-color: green;";?>">
+    <?php if(isset($_GET["id"])) echo ($occupied) ? "Belegt" : "Frei"; else echo "Free/Occupied"?>
+    </button>
   </div>
+<script>
+  
+  const roomname = $("#constRoomname").val();
+
+  function change(){
+    var status = $(".line").css("background-color");
+
+    //"rgb(139, 0, 0)" means darkred/occupied
+    if(status === "rgb(139, 0, 0)"){
+      $(".line").css("background-color", "green");
+      $("#switch").css("background-color", "green");
+      $("#switch").css("border-color", "green");
+      $("#roomname").html(roomname + " - Frei");
+      $("#switch").html("Frei");
+
+    //rgb(0, 128, 0) means green/free
+    } else if (status === "rgb(0, 128, 0)"){
+      $(".line").css("background-color", "darkred");
+      $("#switch").css("background-color", "darkred");
+      $("#switch").css("border-color", "darkred");
+      $("#roomname").html(roomname + " - Belegt");
+      $("#switch").html("Belegt");
+    }
+  }
+
+</script>
 <!-- End Document
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 </body>
